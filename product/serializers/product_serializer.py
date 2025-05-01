d8cb9abe53112f52214d9bf3c80a69ff605de0e0
Fábio Santos <fabiotrabmat@gmail.com>
2025-05-01 19:32:18 -0300

    feat: adicionando viewsets e realizando testes de rotas

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -5,6 +5,51 @@ from .category_serializer import CategorySerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(required=False, many=True)
+    categories_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), write_only=True, many=True
+    )
+
+    def create(self, validated_data):
+        categories_data = validated_data.pop("categories_id")
+
+        product = Product.objects.create(**validated_data)
+
+        for category_data in categories_data:
+            product.category.add(category_data)
+
+        return product
+
+    def update(self, instance, validated_data):
+        categories_data = validated_data.pop("category", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if categories_data is not None:
+            instance.category.clear()
+            for category_data in categories_data:
+                category, _ = Category.objects.get_or_create(
+                    slug=category_data.get("slug"), defaults=category_data
+                )
+            instance.category.add(category)
+        return instance
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "description",
+            "price",
+            "active",
+            "category",
+            "categories_id",
+        ]
+        extra_kwargs = {"category": {"required": False}}
+
+
+class ProductSerializerV2(serializers.ModelSerializer):
     category = CategorySerializer(required=True, many=True)
 
     def create(self, validated_data):
---
+++ product/tests/test_product_viewset.py
@@ -0,0 +1,41 @@
+import json
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from django.urls import reverse
+
+from product.factories import CategoryFactory, ProductFactory
+from order.factories import UserFactory
+from product.models import Product
+
+
+class TestProductViewSet(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.product = ProductFactory(title="Teclado", price="1545.60")
+
+    def test_get_all_product(self):
+        response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        product_data = json.loads(response.content)
+
+        self.assertEqual(product_data[0]["title"], self.product.title)
+        self.assertEqual(product_data[0]["price"], float(self.product.price))
+        self.assertEqual(product_data[0]["active"], self.product.active)
+
+    def test_create_product(self):
+        category = CategoryFactory()
+        data = {"title": "Notebook", "price": 3799.99, "categories_id": [category.id]}
+        response = self.client.post(
+            reverse("product-list", kwargs={"version": "v1"}),
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        created_product = Product.objects.get(title="Notebook")
+
+        self.assertEqual(created_product.title, "Notebook")
+        self.assertEqual(float(created_product.price), 3799.99)
