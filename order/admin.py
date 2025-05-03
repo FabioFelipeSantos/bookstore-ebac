93388c349017982271a5c9616b14c22ccece3cda
Fábio Santos <fabiotrabmat@gmail.com>
2025-05-03 19:27:48 -0300

    feat: adicionando páginas ao admin

--- order/admin.py
+++ order/admin.py
@@ -2,3 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user__username",
+        "product__title",
+    )
+    list_filter = ("product__active",)
+    search_fields = ("user__username", "product__name")
+    ordering = ("-id", "product__title")
+    list_per_page = 10
+
+
+admin.site.register(Order, OrderAdmin)
